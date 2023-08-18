aa8f8cc43bf806f447d2a167d2e72e1cd890699c
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-08-18 09:07:18 +0900

    Fix :: File Logic

---
+++ portfolio/src/pages/MainPage.js
@@ -0,0 +1,15 @@
+import Main from "../components/MainPage/main/Main.jsx";
+import Footer from "../components/common/footer/Footer.jsx";
+import Header from "../components/common/header/Header.jsx";
+
+function MainPage() {
+  return (
+    <>
+    <Header/>
+     <Main/>
+     <Footer/>
+    </>
+  );
+}
+
+export default MainPage;
---
+++ portfolio/src/pages/Project.js
@@ -0,0 +1,14 @@
+import Main from "../components/ProjectPage/main/Main.jsx";
+import Footer from "../components/common/footer/Footer.jsx";
+import Header from "../components/common/header/Header.jsx";
+
+function Project(){
+    return(
+        <> 
+            <Header/>
+            <Main/>
+            <Footer/>
+        </>
+    )
+}
+export default Project;
