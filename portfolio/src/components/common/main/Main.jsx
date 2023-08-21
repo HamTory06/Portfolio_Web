4397d2b6345128f86904baa94c965b005c5ee025
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-08-21 20:27:36 +0900

    💄 stacks design

--- portfolio/src/components/common/main/Main.jsx
+++ portfolio/src/components/common/main/Main.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import "./Main.css"
-
+import Android_icon from '../../../asset/main/androidstdio_icon.png'
+import SpringBoot_icon from '../../../asset/main/SpringBoot_icon.png'
+import Kotlin_icon from '../../../asset/main/Kotlin_icon.png'
+import React_icon from '../../../asset/main/React-icon.png'
+import Web_icon from '../../../asset/main/Web_icon.png'
 
 
 const Main = (props) => {
@@ -8,22 +12,44 @@ const Main = (props) => {
      <div className='root'>
 
         <div className='home' ref={props.Refs.home}>
-         <h1 className='text'>경험하며 레벨업하는 개발자</h1>
+         <h1>경험하며 레벨업하는 개발자</h1>
         </div>
 
-        <div className='stacks' ref={props.Refs.stacks}>
+        <div className='page' ref={props.Refs.stacks}>
+            <div className='stacksWrap'>
+                <h2>Android</h2>
+                <div className='imgWrap'>
+                    <img src={Kotlin_icon} alt="Kotlin" />
+                    <img src={Android_icon} alt="Android"/>
+                </div>
+            </div>
+
+            <div className='stacksWrap'>
+                <h2>Backend</h2>
+                <div className='imgWrap'>
+                <img src={Kotlin_icon} alt="Kotlin" />
+                <img src={SpringBoot_icon} alt="SpringBoot" />
+                </div>
+            </div>
+
+            <div className='stacksWrap'>
+                <h2>Frontend</h2>
+                <div className='imgWrap'>
+                    <img src={Web_icon} alt="Web" className='img'/>
+                    <img src={React_icon} alt="React" />
+                </div>
+            </div>
+        </div>
+
+        <div className='page' ref={props.Refs.project}>
             
         </div>
 
-        <div className='project' ref={props.Refs.project}>
+        <div className='page' ref={props.Refs.contest}>
             
         </div>
 
-        <div className='contest' ref={props.Refs.contest}>
-            
-        </div>
-
-        <div className='activity' ref={props.Refs.activity}>
+        <div className='page' ref={props.Refs.activity}>
             
         </div>
         
